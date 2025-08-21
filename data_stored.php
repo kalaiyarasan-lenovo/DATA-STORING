2af018ad0b115dcac3cbbc0007e689c0f5e9479b
kalaiyarasan-lenovo <kalaivelliangiri2gmail.com>
2025-08-21 11:07:05 +0530

    update

--- data_stored.php
+++ data_stored.php
@@ -37,6 +37,7 @@ if(isset( $_POST["submit"])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Deatils</title>
+    <link rel="stylesheet" href="login_style">
 </head>
 <body>
     <form action="data_stored.php" method="post">
